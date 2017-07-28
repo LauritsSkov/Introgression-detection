255e7bc33dcf73f8d90a832364a3fda82f745fe0
LauritsSkov <30321818+LauritsSkov@users.noreply.github.com>
2017-07-28 16:53:24 +0200

    Update Decode.py

--- Decode.py
+++ Decode.py
@@ -16,9 +16,10 @@ with open(outprefix + '.All_posterior_probs.txt','w') as posterior_sequence, ope
     counter = 1
     start = 1
     snp_counter = 0
+    total_prob = 0.0
 
     # Make headers
-    summary.write('name\tstart\tend\tlength\tstate\tsnps\n')
+    summary.write('name\tstart\tend\tlength\tstate\tsnps\tmean_prob\n')
     posterior_sequence.write('observations\t{}\n'.format('\t'.join(state_names)))
     posterior_sequence_mostlikely.write('observations\tstate\n')
 
@@ -32,6 +33,7 @@ with open(outprefix + '.All_posterior_probs.txt','w') as posterior_sequence, ope
 
         snps = x
         current_seg = state_names[index]
+        
 
         if i > 0:
 
@@ -39,17 +41,22 @@ with open(outprefix + '.All_posterior_probs.txt','w') as posterior_sequence, ope
             if current_seg == previos_seg:
                 counter += 1
                 snp_counter += int(snps)
-
+                total_prob += value
 
             # Begin new block
             if current_seg != previos_seg:
-                summary.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\n'.format(infile, start, i, counter, previos_seg, snp_counter)) 
+
+                print total_prob, counter
+                mean_prob = total_prob / counter
+                summary.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\n'.format(infile, start, i, counter, previos_seg, snp_counter, mean_prob)) 
                 
                 counter = 1
                 start = i + 1
                 snp_counter = int(snps)
+                total_prob = value
+
 
         previos_seg = current_seg
 
-
-    summary.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\n'.format(infile, start, i+1, counter, previos_seg, snp_counter))       
+    mean_prob = total_prob / counter
+    summary.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\n'.format(infile, start, i+1, counter, previos_seg, snp_counter, mean_prob))       
