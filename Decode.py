aafbaead3bd38333332e652d39c5a5d8919d4988
LauritsSkov <30321818+LauritsSkov@users.noreply.github.com>
2019-06-11 12:35:44 +0200

    Update Decode.py
    Can now handle windows of length 1.

--- Decode.py
+++ Decode.py
@@ -9,9 +9,6 @@ window_size = int(window_size)
 state_names, transitions, emissions, starting_probabilities, weights, mutrates = make_hmm_from_file(model, weights_file, mutfile) 
 obs, chroms, starts, variants = read_observations_from_file(infile)
 
-
-
-
 # Posterior decode the file
 post_seq = Forward_backward(starting_probabilities, transitions, emissions, weights, obs, mutrates)
 
@@ -25,7 +22,6 @@ with open(outprefix + '.All_posterior_probs.txt','w') as posterior_sequence, ope
     total_prob = 0.0
     
     start = 0
-    end = 0
 
     # Make headers
     summary.write('name\tchrom\tstart\tend\tlength\tstate\tsnps\tmean_prob\n')
@@ -33,40 +29,46 @@ with open(outprefix + '.All_posterior_probs.txt','w') as posterior_sequence, ope
 
 
 
-    for i, (x,chrom, current_start, var) in enumerate(zip(obs, chroms, starts, variants)):
+    for i, (x,chrom, currentstart, var) in enumerate(zip(obs, chroms, starts, variants)):
 
         v = post_seq[:,i]
+
         index, value = max(enumerate([float(y) for y in v]), key=operator.itemgetter(1))
-        posterior_sequence.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\n'.format(chrom, current_start, x, var, state_names[index], '\t'.join([str(val) for val in v]) ))
+        posterior_sequence.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\n'.format(chrom, currentstart, x, var, state_names[index], '\t'.join([str(val) for val in v]) ))
 
         snps = x
         current_seg = state_names[index]       
 
-        if i > 0:
+       
 
-            # Extend block
-            if current_seg == previos_seg and chrom == previous_chrom:
-                counter += 1
-                snp_counter += int(snps)
-                total_prob += value
-                end = current_start
+        # Extend block
+        if current_seg == previos_seg and chrom == previous_chrom:
+            counter += 1
+            snp_counter += int(snps)
+            total_prob += value
 
-            # Begin new block
-            if current_seg != previos_seg or chrom != previous_chrom:
-
+        # Begin new block
+        if current_seg != previos_seg or chrom != previous_chrom:
+            
+            # Write previous block to output
+            if i > 0:
                 mean_prob = total_prob / counter
-                summary.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\n'.format(outprefix, previous_chrom, start, end, end - start + window_size, previos_seg, snp_counter, mean_prob)) 
-                
-                counter = 1
-                start = current_start
-                snp_counter = int(snps)
-                total_prob = value
+                end = starts[i-1] + window_size
+                summary.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\n'.format(outprefix, previous_chrom, start, end, end - start, previos_seg, snp_counter, mean_prob)) 
+            
+            # Keep track of length, number of snps and prob of new block
+            counter = 1
+            start = currentstart
+            snp_counter = int(snps)
+            total_prob = value
 
 
+       
         previos_seg = current_seg
         previous_chrom = chrom
 
 
-
+    # when the file is done write the last segment
     mean_prob = total_prob / counter
-    summary.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\n'.format(outprefix, previous_chrom, start, end, end - start + window_size, previos_seg, snp_counter, mean_prob)) 
+    end = currentstart + window_size
+    summary.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\n'.format(outprefix, previous_chrom, start, end, end - start, previos_seg, snp_counter, mean_prob)) 
