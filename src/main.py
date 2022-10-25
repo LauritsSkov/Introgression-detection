dced1cc71ec237b325c4e4b04dfce0d85baf4e87
LauritsSkov <30321818+LauritsSkov@users.noreply.github.com>
2022-10-24 20:31:54 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,12 +5,15 @@ from hmm_functions import TrainModel, DecodeModel, write_HMM_to_file, read_HMM_p
 from bcf_vcf import make_out_group, make_ingroup_obs
 from make_test_data import create_test_data
 from make_mutationrate import make_mutation_rate
-from helper_functions import Load_observations_weights_mutrates
+from helper_functions import Load_observations_weights_mutrates, handle_individuals_input, handle_infiles, combined_files
+
+
+VERSION = '0.6.0'
 
 
 def print_script_usage():
-    toprint = '''
-Script for identifying introgressed archaic segments
+    toprint = f'''
+Script for identifying introgressed archaic segments (version: {VERSION})
 
 > Turorial:
 hmmix make_test_data 
@@ -50,7 +53,7 @@ Different modes (you can also see the options for each by writing hmmix make_tes
     -weights            file with callability (defaults to all positions being called)
     -mutrates           file with mutation rates (default is mutation rate is uniform)
     -param              markov parameters file (default is human/neanderthal like parameters)
-    -out                outputfile prefix (default is a file named trained.json)
+    -out                outputfile (default is a file named trained.json)
     -window_size        size of bins (default is 1000 bp)
     -haploid            Change from using diploid data to haploid data (default is diploid)
 
@@ -59,10 +62,11 @@ Different modes (you can also see the options for each by writing hmmix make_tes
     -weights            file with callability (defaults to all positions being called)
     -mutrates           file with mutation rates (default is mutation rate is uniform)
     -param              markov parameters file (default is human/neanderthal like parameters)
-    -out                outputfile prefix (default is stdout)
+    -out                outputfile prefix <out>.hap1.txt and <out>.hap2.txt if -haploid option is used or <out>.diploid.txt (default is stdout)
     -window_size        size of bins (default is 1000 bp)
     -haploid            Change from using diploid data to haploid data (default is diploid)
     -admixpop ADMIXPOP  Annotate using vcffile with admixing population (default is none)
+    -extrainfo          Add archaic information on each SNP (default is off)
     '''
 
     return toprint
@@ -112,7 +116,7 @@ def main():
     train_subparser.add_argument("-weights", metavar='',help="file with callability (defaults to all positions being called)")
     train_subparser.add_argument("-mutrates", metavar='',help="file with mutation rates (default is mutation rate is uniform)")
     train_subparser.add_argument("-param", metavar='',help="markov parameters file (default is human/neanderthal like parameters)", type=str)
-    train_subparser.add_argument("-out", metavar='',help="outputfile prefix (default is a file named trained.json)", default = 'trained.json')
+    train_subparser.add_argument("-out", metavar='',help="outputfile (default is a file named trained.json)", default = 'trained.json')
     train_subparser.add_argument("-window_size", metavar='',help="size of bins (default is 1000 bp)", type=int, default = 1000)
     train_subparser.add_argument("-haploid",help="Change from using diploid data to haploid data (default is diploid)", action='store_true', default = False)
 
@@ -122,25 +126,30 @@ def main():
     decode_subparser.add_argument("-weights", metavar='',help="file with callability (defaults to all positions being called)")
     decode_subparser.add_argument("-mutrates", metavar='',help="file with mutation rates (default is mutation rate is uniform)")
     decode_subparser.add_argument("-param", metavar='',help="markov parameters file (default is human/neanderthal like parameters)", type=str)
-    decode_subparser.add_argument("-out", metavar='',help="outputfile prefix (default is stdout)", default = '/dev/stdout')
+    decode_subparser.add_argument("-out", metavar='',help="outputfile prefix <out>.hap1.txt and <out>.hap2.txt if -haploid option is used or <out>.diploid.txt (default is stdout)", default = '/dev/stdout')
     decode_subparser.add_argument("-window_size", metavar='',help="size of bins (default is 1000 bp)", type=int, default = 1000)
     decode_subparser.add_argument("-haploid",help="Change from using diploid data to haploid data (default is diploid)", action='store_true', default = False)
     decode_subparser.add_argument("-admixpop",help="Annotate using vcffile with admixing population (default is none)")
+    decode_subparser.add_argument("-extrainfo",help="Add archaic information on each SNP", action='store_true', default = False)
 
 
     args = parser.parse_args()
 
-
+    # Make test data
+    # ------------------------------------------------------------------------------------------------------------
     if args.mode == 'make_test_data':
         create_test_data(data_set_length = args.windows, write_out_files = args.nooutfiles)
 
 
+
+
+    # Train parameters
+    # ------------------------------------------------------------------------------------------------------------
     elif args.mode == 'train':
 
         hmm_parameters = read_HMM_parameters_from_file(args.param)
         obs, _, _, _, mutrates, weights = Load_observations_weights_mutrates(args.obs, args.weights, args.mutrates, args.window_size, args.haploid)
         
-        # Print parameters to screen
         print('-' * 40)
         print(hmm_parameters)
         print('> number of windows:', len(obs), '. Number of snps = ', sum(obs))
@@ -155,36 +164,95 @@ def main():
         write_HMM_to_file(hmm_parameters, args.out)
 
 
+
+
+    # Decode observations using parameters
+    # ------------------------------------------------------------------------------------------------------------
     elif args.mode == 'decode':
 
         obs, chroms, starts, variants, mutrates, weights  = Load_observations_weights_mutrates(args.obs, args.weights, args.mutrates, args.window_size, args.haploid)
         hmm_parameters = read_HMM_parameters_from_file(args.param)
         
-        # Print parameters to screen
         print('-' * 40)
         print(hmm_parameters)  
         print('> number of windows:', len(obs), '. Number of snps = ', sum(obs))
         print('> total callability:', round(np.sum(weights) / len(obs),2) )
         print('> average mutation rate per bin:', round(np.sum(mutrates * weights) / np.sum(weights), 2) )
-        print('> Output is',args.out) 
+        print('> Output prefix is',args.out) 
         print('> Window size is',args.window_size, 'bp') 
         print('> Haploid',args.haploid) 
         print('-' * 40)
 
         # Find segments and write output
         segments = DecodeModel(obs, chroms, starts, variants, mutrates, weights, hmm_parameters)
-        Write_Decoded_output(args.out, segments, args.obs, args.admixpop)
+        Write_Decoded_output(args.out, segments, args.obs, args.admixpop, args.extrainfo)
 
 
 
+
+
+    # Create outgroup snps (set of snps to be removed)
+    # ------------------------------------------------------------------------------------------------------------
     elif args.mode == 'create_outgroup':
-        make_out_group(args.ind, args.weights, args.vcf, args.out, args.ancestral, args.refgenome)
+
+        # Get list of outgroup individuals
+        outgroup_individuals = handle_individuals_input(args.ind, 'outgroup')
+
+        # Get a list of vcffiles and ancestral files and intersect them
+        vcffiles = handle_infiles(args.vcf)
+        ancestralfiles = handle_infiles( args.ancestral)
+        refgenomefiles = handle_infiles(args.refgenome)
+
+        ancestralfiles, vcffiles = combined_files(ancestralfiles, vcffiles)
+        refgenomefiles, vcffiles = combined_files(refgenomefiles, vcffiles)
+
+        print('-' * 40)
+        print('> Outgroup individuals:', len(outgroup_individuals))
+        print('> Using vcf and ancestral files')
+        for vcffile, ancestralfile, reffile in zip(vcffiles, ancestralfiles, refgenomefiles):
+            print('vcffile:',vcffile, 'ancestralfile:',ancestralfile, 'reffile:', reffile)
+        print()    
+        print('> Callability file:',  args.weights)
+        print(f'> Writing output to:', args.out)
+        print('-' * 40)
 
 
+        make_out_group(outgroup_individuals, args.weights, vcffiles, args.out, ancestralfiles, refgenomefiles)
+
+
+
+
+    # Create ingroup observations
+    # ------------------------------------------------------------------------------------------------------------
     elif args.mode == 'create_ingroup':
-        make_ingroup_obs(args.ind, args.weights, args.vcf, args.out, args.outgroup, args.ancestral)
+
+        # Get a list of ingroup individuals
+        ingroup_individuals = handle_individuals_input(args.ind,'ingroup')
+
+        # Get a list of vcffiles and ancestral files and intersect them
+        vcffiles = handle_infiles(args.vcf)
+        ancestralfiles = handle_infiles(args.ancestral)
+        ancestralfiles, vcffiles  = combined_files(ancestralfiles, vcffiles)
+
+        print('-' * 40)
+        print('> Ingroup individuals:', len(ingroup_individuals))
+        print('> Using vcf and ancestral files')
+        for vcffile, ancestralfile in zip(vcffiles, ancestralfiles):
+            print('vcffile:',vcffile, 'ancestralfile:',ancestralfile)
+        print()  
+        print('> Using outgroup variants from:', args.outgroup)  
+        print('> Callability file:', args.weights)
+        print(f'> Writing output to file with prefix: {args.out}.<individual>.txt')
+        print('-' * 40)
 
 
+        make_ingroup_obs(ingroup_individuals, args.weights, vcffiles, args.out, args.outgroup, ancestralfiles)
+
+
+
+
+    # Estimate mutation rate
+    # ------------------------------------------------------------------------------------------------------------
     elif args.mode == 'mutation_rate':
         print('-' * 40)
         print(f'> Outgroupfile:', args.outgroup)
@@ -194,6 +262,13 @@ def main():
         print('-' * 40)
 
         make_mutation_rate(args.outgroup, args.out, args.weights, args.window_size)
+    
+    
+
+
+
+    # Print usage
+    # ------------------------------------------------------------------------------------------------------------
     else:
         print(print_script_usage())
 
