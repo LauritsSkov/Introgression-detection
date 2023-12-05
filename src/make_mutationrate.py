c8e625d7f0598a28affa853281691868be78b55b
LauritsSkov <30321818+LauritsSkov@users.noreply.github.com>
2023-12-05 14:20:59 -0800

    Update make_mutationrate.py

--- src/make_mutationrate.py
+++ src/make_mutationrate.py
@@ -49,4 +49,3 @@ def make_mutation_rate(freqfile, outfile, callablefile, window_size):
                 ratio = round(mut/call/genome_mean, 2)
 
             print(chrom, start, end, ratio, sep = '\t', file = out)
-            
