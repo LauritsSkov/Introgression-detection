44b09c76dd3e96eee27d3a1e980a5e63539cf113
LauritsSkov <30321818+LauritsSkov@users.noreply.github.com>
2024-05-15 15:48:55 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='hmmix', 
-    version = '0.6.9',
+    version = '0.7.0',
     description='Find introgressed segments',
     py_modules=['bcf_vcf', 'helper_functions', 'hmm_functions', 'main', 'make_mutationrate', 'make_test_data'],
     package_dir={'': 'src'},
