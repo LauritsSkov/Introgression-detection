8baa43daf2cbd326cff2e3f9585cd9d1accd98cc
LauritsSkov <30321818+LauritsSkov@users.noreply.github.com>
2024-11-25 07:33:20 -0800

    Update helper_functions.py

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -2,8 +2,6 @@ import numpy as np
 import json
 from collections import defaultdict
 import os, sys
-import itertools
-import difflib
 from glob import glob
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -14,36 +12,37 @@ def make_callability_from_bed(bedfile, window_size):
     with open(bedfile) as data:
         for line in data:
 
-            if not line.startswith('chrom'):
+            if line.startswith('chrom'):
+                continue
 
-                if len(line.strip().split('\t')) == 3:
-                    chrom, start, end = line.strip().split('\t')
-                    value = 1
-                elif  len(line.strip().split('\t')) > 3:
-                    chrom, start, end, value = line.strip().split('\t')[0:4]
-                    value = float(value)
+            if len(line.strip().split('\t')) == 3:
+                chrom, start, end = line.strip().split('\t')
+                value = 1
+            elif  len(line.strip().split('\t')) > 3:
+                chrom, start, end, value = line.strip().split('\t')[0:4]
+                value = float(value)
 
-                start, end = int(start), int(end)
+            start, end = int(start), int(end)
 
-                firstwindow = start - start % window_size
-                lastwindow = end - end % window_size
-                
-                # not spanning multiple windows (all is added to same window)
-                if firstwindow == lastwindow:
-                    callability[chrom][firstwindow] += (end-start+1) * value
+            firstwindow = start - start % window_size
+            lastwindow = end - end % window_size
+            
+            # not spanning multiple windows (all is added to same window)
+            if firstwindow == lastwindow:
+                callability[chrom][firstwindow] += (end-start+1) * value
 
-                # spanning multiple windows
-                else:
-                    # add to end windows
-                    firstwindow_fill = window_size - start % window_size
-                    lastwindow_fill = end %window_size
-                
-                    callability[chrom][firstwindow] += firstwindow_fill * value
-                    callability[chrom][lastwindow] += (lastwindow_fill+1) * value
+            # spanning multiple windows
+            else:
+                # add to end windows
+                firstwindow_fill = window_size - start % window_size
+                lastwindow_fill = end %window_size
+            
+                callability[chrom][firstwindow] += firstwindow_fill * value
+                callability[chrom][lastwindow] += (lastwindow_fill+1) * value
 
-                    # fill in windows in the middle
-                    for window_tofil in range(firstwindow + window_size, lastwindow, window_size):
-                        callability[chrom][window_tofil] += window_size * value
+                # fill in windows in the middle
+                for window_tofil in range(firstwindow + window_size, lastwindow, window_size):
+                    callability[chrom][window_tofil] += window_size * value
 
     return callability
 
@@ -51,82 +50,79 @@ def make_callability_from_bed(bedfile, window_size):
 
 def Load_observations_weights_mutrates(obs_file, weights_file, mutrates_file, window_size = 1000, haploid = False, chrom_to_look_for = 'All'):
 
-    obs_counter = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
-    haplotypes = defaultdict(int)
-
-    if chrom_to_look_for != 'All':
-        chromosome_list = chrom_to_look_for.split(',')
-
+    # get span of data
+    chromosome_spans = defaultdict(int)
     with open(obs_file) as data:
         for line in data:
-            if not line.startswith('chrom'):
-                chrom, pos, ancestral_base, genotype = line.strip().split()
+            if line.startswith('chrom'):
+                continue
+            chrom, pos, ancestral_base, genotype = line.strip().split()
 
-               
-                
-                keep_chromosome = False
-                if chrom_to_look_for == 'All':
-                    keep_chromosome = True
-                else:
-                    if chrom in chromosome_list:
-                        keep_chromosome = True
-
-                if keep_chromosome:
-                     # convert 1-indexed position to 0-indexed position
-                    zero_based_pos = int(pos) - 1
-                    rounded_pos = zero_based_pos - zero_based_pos % window_size
-
-                    if haploid:  
-                        for i, base in enumerate(genotype):
-                            if base != ancestral_base:
-                                obs_counter[chrom][rounded_pos][f'_hap{i+1}'].append(pos)
-                                haplotypes[f'_hap{i+1}'] += 1
-                    else:
-                        obs_counter[chrom][rounded_pos][''].append(pos)
-                        haplotypes[''] += 1
+            # convert 1-indexed position to 0-indexed position
+            zero_based_pos = int(pos) - 1
+            rounded_pos = zero_based_pos - zero_based_pos % window_size
+            chromosome_spans[chrom] = rounded_pos
 
 
-    chroms, starts, variants, obs = [], [], [], []
-    # In the case that there are NO derived variants - use weights to make list of zeros
-    if len(obs_counter) == 0:
-
-        if weights_file is None:
-            sys.exit(f'{obs_file} is empty! You need to provide a bed file!')
-
-        haplotypes[''] += 1
+    # get span of callability
+    if weights_file: 
         callability = make_callability_from_bed(weights_file, window_size)
-        for chrom in sorted(callability, key=sortby):
+        for chrom in callability:
+            chromosome_spans[chrom] = max(callability[chrom])
 
-            keep_variant = False
-            if chrom_to_look_for == 'All':
-                keep_variant = True
-            else:
-                if chrom in chromosome_list:
-                    keep_variant = True
-
-            if keep_variant:
-                lastwindow = max(callability[chrom]) + window_size
-
-                for window in range(0, lastwindow, window_size):
-                    obs_counter[chrom][window][''].append('')
-                    chroms.append(f'{chrom}')   
-                    starts.append(window)
-                    variants.append('')  
-                    obs.append(0) 
-
-    # Otherwise fill out as normal
+    # which chromosomes are we interested in
+    if chrom_to_look_for != 'All':
+        chromosome_list = sorted(chrom_to_look_for.split(','), key=sortby)
     else:
-        for haplotype in sorted(haplotypes, key=sortby_haplotype):
-            
-            for chrom in sorted(obs_counter, key=sortby):
-                lastwindow = max(obs_counter[chrom]) + window_size
+        chromosome_list = sorted(list(chromosome_spans.keys()), key=sortby)
 
-                for window in range(0, lastwindow, window_size):
-                    chroms.append(f'{chrom}{haplotype}')   
-                    starts.append(window)
-                    variants.append(','.join(obs_counter[chrom][window][haplotype]))  
-                    obs.append(len(obs_counter[chrom][window][haplotype])) 
-                
+    obs_counter = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+    haplotypes = defaultdict(int)
+    chroms, starts, variants, obs = [], [], [], []
+
+    # read observation data
+    with open(obs_file) as data:
+        for line in data:
+            if line.startswith('chrom'):
+                continue 
+
+            chrom, pos, ancestral_base, genotype = line.strip().split()
+            if not chrom in chromosome_list:
+                continue
+
+            # convert 1-indexed position to 0-indexed position
+            zero_based_pos = int(pos) - 1
+            rounded_pos = zero_based_pos - zero_based_pos % window_size
+
+            if haploid:  
+                for i, base in enumerate(genotype):
+                    if base != ancestral_base:
+                        obs_counter[chrom][rounded_pos][f'_hap{i+1}'].append(pos)
+                        haplotypes[f'_hap{i+1}'] += 1
+            else:
+                obs_counter[chrom][rounded_pos][''].append(pos)
+                haplotypes[''] += 1
+
+
+    # take care of cases where there is 0 observations
+    if len(haplotypes) == 0:
+        if haploid:
+            sys.exit('Could not determine haploidity because there is no data')
+        else:
+            haplotypes[''] += 1
+        
+
+    for haplotype in sorted(haplotypes, key=sortby_haplotype):
+        
+        for chrom in chromosome_list:
+            lastwindow = chromosome_spans[chrom]
+
+            for window in range(0, lastwindow, window_size):
+                chroms.append(f'{chrom}{haplotype}')   
+                starts.append(window)
+                variants.append(','.join(obs_counter[chrom][window][haplotype]))  
+                obs.append(len(obs_counter[chrom][window][haplotype])) 
+            
 
     # Read weights file is it exists - else set all weights to 1
     if weights_file is None:
@@ -136,8 +132,8 @@ def Load_observations_weights_mutrates(obs_file, weights_file, mutrates_file, wi
         weights = []
         for haplotype in sorted(haplotypes, key=sortby_haplotype):
             
-            for chrom in sorted(obs_counter, key=sortby):
-                lastwindow = max(obs_counter[chrom]) + window_size
+            for chrom in chromosome_list:
+                lastwindow = chromosome_spans[chrom]
 
                 for window in range(0, lastwindow, window_size):
                     weights.append(callability[chrom][window] / float(window_size))
@@ -151,11 +147,13 @@ def Load_observations_weights_mutrates(obs_file, weights_file, mutrates_file, wi
         mutrates = []
         for haplotype in sorted(haplotypes, key=sortby_haplotype):
             
-            for chrom in sorted(obs_counter, key=sortby):
-                lastwindow = max(obs_counter[chrom]) + window_size
+            for chrom in chromosome_list:
+                lastwindow = chromosome_spans[chrom]
 
                 for window in range(0, lastwindow, window_size):
                     mutrates.append(callability[chrom][window] / float(window_size))
+
+
 
     # Make sure there are no places with obs > 0 and 0 in mutation rate or weight
     for index, (observation, w, m) in enumerate(zip(obs, weights, mutrates)):
@@ -163,7 +161,6 @@ def Load_observations_weights_mutrates(obs_file, weights_file, mutrates_file, wi
             print(f'warning, you had {observation} observations but no called bases/no mutation rate at index:{index}. weights:{w}, mutrates:{m}')
             obs[index] = 0
             
-
     return np.array(obs).astype(int), chroms, starts, variants, np.array(mutrates).astype(float), np.array(weights).astype(float)
 
 
@@ -224,53 +221,6 @@ def sortby(x):
     else:
         return 3e6
     
-
-def get_consensus(infiles):
-    '''
-    Find consensus prefix, postfix and value that changes in set of files:
-
-    myfiles = ['chr1.vcf', 'chr2.vcf', 'chr3.vcf']
-    prefix, postfix, values = get_consensus(myfiles) 
-    
-    prefix=chr
-    postfix=.vcf
-    values = {1,2,3} -> is a set
-    '''
-    infiles = [str(x) for x in infiles]
-
-    if len(infiles) > 1:
-
-        consensus_strings = defaultdict(int)
-        for a, b in itertools.combinations(infiles,2):
-            consensus_a = 'START'
-            for i,s in enumerate(difflib.ndiff(a, b)):
-                if s[0] != ' ':
-                    consensus_a += ' '
-                else:
-                    consensus_a += s[-1]
-            consensus_a += 'END'
-
-
-            new_joined = '|'.join(consensus_a.split()).replace('START','').replace('END','')
-            consensus_strings[new_joined] += 1
-
-        for value in consensus_strings:
-
-            if len(value.split('|')) == 2:
-                prefix, postfix = value.split('|')
-                matches = len([x for x in infiles if prefix in x and postfix in x])
-
-                if matches == len(infiles):
-                    values = set([x.replace(prefix, '').replace(postfix,'') for x in infiles])
-                    return prefix, postfix, sorted(values, key=sortby)
-    else:
-        return None, None, None
-
-
-    
-
-
-
 def sortby_haplotype(x):
     '''
     This function will sort haplotypes by number
@@ -372,16 +322,7 @@ def handle_individuals_input(argument, group_to_choose):
         return argument.split(',')
 
 
-# Check which type of input we are dealing with
-def handle_infiles(input):
-    file_list = glob(input)
-    if len(file_list) > 0:
-        return file_list
-    else:
-        if ',' in input:
-            return input.split(',')
-        else:
-            return [input]
+
 
 # Clean up
 def clean_files(filename):
@@ -391,17 +332,8 @@ def clean_files(filename):
 
 # Find variants from admixed population
 def flatten_list(variants_list):
+    return ','.join([x for x in variants_list if x != ''])
 
-    flattened_list = []
-    for bin in variants_list:
-        if bin != '':
-            if ',' in bin:
-                for position in bin.split(','):
-                    flattened_list.append(position)
-            else:
-                flattened_list.append(bin)
-
-    return ','.join(flattened_list)
 
 
 def convert_to_bases(genotype, both_bases):
@@ -424,6 +356,53 @@ def convert_to_bases(genotype, both_bases):
 
 
 
+
+def get_consensus(infiles):
+    '''
+    Find consensus prefix, suffix and value that changes in set of files:
+
+    myfiles = ['chr1.vcf', 'chr2.vcf', 'chr3.vcf']
+    prefix, suffix, values = get_consensus(myfiles) 
+    
+    prefix  = chr
+    suffix = .vcf
+    values  = [1,2,3] 
+    '''
+    infiles = [str(x) for x in infiles]
+
+    if len(infiles) <= 1:
+        return None, None, None
+    
+    # find longest common prefix
+    prefix = infiles[0]
+    for s in infiles[1:]:
+        while not s.startswith(prefix):
+            prefix = prefix[:-1]
+
+    # find longest common suffix
+    suffix = infiles[0]
+    for s in infiles[1:]:
+        while not s.endswith(suffix):
+            suffix = suffix[1:]
+
+    values = [x[len(prefix):-len(suffix)] if suffix else x[len(prefix):] for x in infiles]
+
+    return prefix, suffix, sorted(values, key=sortby)
+
+
+# Check which type of input we are dealing with
+def handle_infiles(input):
+
+    file_list = glob(input)
+    if len(file_list) > 0:
+        return file_list
+    else:
+        return input.split(',')
+    
+
+
+    
+
 # Check which type of input we are dealing with
 def combined_files(ancestralfiles, vcffiles):
 
@@ -434,14 +413,17 @@ def combined_files(ancestralfiles, vcffiles):
     prefix1, postfix1, values1 = get_consensus(vcffiles)
     prefix2, postfix2, values2 = get_consensus(ancestralfiles)
 
+
     # No ancestral files
     if ancestralfiles == ['']:
+
         ancestralfiles = [None for _ in vcffiles]
         vcffiles = [f'{prefix1}{x}{postfix1}' for x in values1]
         return ancestralfiles, vcffiles
 
     # Same length
     elif len(ancestralfiles) == len(vcffiles):
+
         vcffiles = [f'{prefix1}{x}{postfix1}' for x in values1]
         ancestralfiles = [f'{prefix2}{x}{postfix2}' for x in values2]
         return ancestralfiles, vcffiles
@@ -473,7 +455,7 @@ def combined_files(ancestralfiles, vcffiles):
     # only one ancestral file and many vcf files
     elif len(ancestralfiles) == 1 and len(vcffiles) > 1:
         vcffiles = []
-        
+       
         for key in values1:
             if key in ancestralfiles[0]:
                 vcffiles.append(''.join([prefix1, key, postfix1]))
